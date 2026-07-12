819ec2f7287fec4e6b65701838c83c5b32bb7c7b
Ahmad-Khalifah-Ihsan <ahmadkhalifah162@gmail.com>
2026-07-12 19:33:00 +0700

    trigger main branch deployment on vercel

--- frontend/src/pages/AccountInfo.tsx
+++ frontend/src/pages/AccountInfo.tsx
@@ -1,3 +1,4 @@
+//Tes biar masuk Github
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ArrowLeft, User, Shield, Clock, Trash2, X, Key } from 'lucide-react';
